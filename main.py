bd643216b5a2536fff314fd36ecdee4714d8d429
semwin2008 <semwin2008@gmail.com>
2026-08-24 16:53:34 +0300

    working bot; issue - tg timeout

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from model.model_interface import get_model_reply
@@ -10,14 +11,17 @@ async def start(update: Update, context):
 async def reply(update: Update, context):
     input_text = update.message.text[8:]
     print(f'model query, message is: {input_text}')
-    reply = get_model_reply(input_text)
-    await update.message.reply_text(reply)
+    
+    # Запускаем синхронную функцию в отдельном потоке
+    reply_text = await asyncio.to_thread(get_model_reply, input_text)
+    
+    await update.message.reply_text(reply_text)
 
 
 if __name__ == "__main__":
-    # I assume that this file contains telegram bot token
     with open('.env/TOKEN', 'r', encoding='utf-8') as file:
         TOKEN = file.readline().strip()
+    
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & filters.Regex(r'tokarev +'), reply))
--- model/model_interface.py
+++ model/model_interface.py
@@ -1,14 +1,31 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from pathlib import Path
+
+MAX_NEW_TOKENS = 30
+TEMPERATURE = 0.7
 
 
 def get_model_reply(text):
     """
         returns a LM reply to the input text as a new message
     """
-    model = AutoModelForCausalLM.from_pretrained("./merged/qwen_merged", device_map="auto")
-    tokenizer = AutoTokenizer.from_pretrained("./merged/qwen_merged")
+    if len(text) > 30 * 4:
+        return "Ты чо охренел ???"
+
+    model_name = "qwen_merged"
+    model_path = Path(__file__).parent / "merged" / model_name
+
+    model = AutoModelForCausalLM.from_pretrained(str(model_path.resolve()), device_map="auto")
+    tokenizer = AutoTokenizer.from_pretrained(str(model_path.resolve()))
 
     # Генерация
     inputs = tokenizer(text, return_tensors="pt").to(model.device)
-    outputs = model.generate(**inputs, max_length=50, do_sample=True, temperature=0.7)
+    outputs = model.generate(
+                **inputs,
+                max_new_tokens=MAX_NEW_TOKENS,
+                do_sample=True,
+                temperature=TEMPERATURE,
+                pad_token_id=tokenizer.pad_token_id,
+                eos_token_id=tokenizer.eos_token_id,
+            )
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
