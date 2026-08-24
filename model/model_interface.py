c2b1826e9c1c72687497ad384f017ce4bdd43aee
semwin2008 <semwin2008@gmail.com>
2026-08-24 14:46:58 +0300

    added model interface

--- model/model_interface.py
+++ model/model_interface.py
@@ -1,7 +1,14 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
 def get_model_reply(text):
     """
         returns a LM reply to the input text as a new message
     """
-    pass
+    model = AutoModelForCausalLM.from_pretrained("./smollm_merged", device_map="auto")
+    tokenizer = AutoTokenizer.from_pretrained("./smollm_merged")
+
+    # Генерация
+    inputs = tokenizer(text, return_tensors="pt").to(model.device)
+    outputs = model.generate(**inputs, max_length=50, do_sample=True, temperature=0.7)
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
